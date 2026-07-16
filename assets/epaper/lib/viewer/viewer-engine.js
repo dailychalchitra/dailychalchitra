78405933b25879f23b5c19df2f51772151baf7f7
Daily Chalchitra <nimushamim46@gmail.com>
2026-07-16 04:24:40 +0400

    Update viewer engine to version 1.1

--- assets/epaper/lib/viewer/viewer-engine.js
+++ assets/epaper/lib/viewer/viewer-engine.js
@@ -1,13 +1,13 @@
 /*
 ======================================================
 Daily Chalchitra ePaper Viewer Engine
-Version : 1.0
+Version : 1.1
 ======================================================
 */
 
 window.DCViewer = {
 
-    version: "1.0",
+    version: "1.1",
 
     issue: null,
 
@@ -21,21 +21,88 @@ window.DCViewer = {
 
     pages: [],
 
-    initialized: false
+    initialized: false,
 
-};
-DCViewer.init = function (issue) {
 
-    this.issue = issue;
+    init: function(issue){
 
-    this.currentPage = 1;
+        this.issue = issue;
 
-    this.zoom = 1;
+        this.pdf = issue.pdf;
 
-    this.initialized = true;
+        this.currentPage = 1;
 
-    console.log("Daily Chalchitra Viewer Started");
+        this.totalPages = issue.pages || 0;
 
-    console.log(issue);
+        this.zoom = 1;
+
+        this.pages = [];
+
+        this.initialized = true;
+
+
+        console.log(
+            "Daily Chalchitra ePaper Viewer Initialized"
+        );
+
+        console.log(
+            "PDF:",
+            this.pdf
+        );
+
+        console.log(
+            "Total Pages:",
+            this.totalPages
+        );
+
+    },
+
+
+    nextPage: function(){
+
+        if(this.currentPage < this.totalPages){
+
+            this.currentPage++;
+
+        }
+
+
+        this.renderInfo();
+
+    },
+
+
+    previousPage: function(){
+
+        if(this.currentPage > 1){
+
+            this.currentPage--;
+
+        }
+
+
+        this.renderInfo();
+
+    },
+
+
+    setZoom: function(value){
+
+        this.zoom = value;
+
+        this.renderInfo();
+
+    },
+
+
+    renderInfo: function(){
+
+        console.log({
+            page: this.currentPage,
+            total: this.totalPages,
+            zoom: this.zoom
+        });
+
+    }
 
 };
