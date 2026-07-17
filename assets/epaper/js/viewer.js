34be57506ec4c025fa102ff2d2648956bbf92e93
Daily Chalchitra <nimushamim46@gmail.com>
2026-07-17 06:16:14 +0400

    Refactor DCViewer initialization logic

--- assets/epaper/js/viewer.js
+++ assets/epaper/js/viewer.js
@@ -171,20 +171,10 @@ HTML ePaper Start
 
 
 if(window.DCViewer){
-if(window.DCViewer){
-
-    console.log("DCViewer Found");
 
     DCViewer.init(issue);
 
-    await DCViewer.start();
-
-    console.log("DCViewer Started");
-
-}
-else{
-
-    console.log("DCViewer Missing");
+    DCViewer.start();
 
 }
 
